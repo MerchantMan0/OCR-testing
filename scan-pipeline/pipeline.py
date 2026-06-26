30a8040874b0fea30401a75574c6181862aab56f
Noah Porter <noahporter86@gmail.com>
2026-06-25 20:47:56 -0400

    add gpu

--- scan-pipeline/pipeline.py
+++ scan-pipeline/pipeline.py
@@ -20,7 +20,7 @@ _model = None
 def _get_model():
     global _model
     if _model is None:
-        _model = ocr_predictor(pretrained=True)
+        _model = ocr_predictor(pretrained=True).cuda() # remove .cuda() to use CPU
     return _model
 
 
